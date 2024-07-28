11e896a1969d0304ad03d3f6a2b48ace1201a272
Eldos Yksanov <eldosyksanov@gmail.com>
2024-07-29 00:03:06 +0600

    added code to Program class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.Net.WebSockets;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography;
 
 namespace OOP_lesson28;
@@ -20,5 +21,22 @@ class Program
             string result = ((ReadState)read)();
             Console.WriteLine(result);
         }
+        
+        Console.WriteLine("Задания 2\n");
+        Console.WriteLine("Отправка сообщения");
+        Console.WriteLine("Введите тип сообщения: ");
+        string type = Console.ReadLine();
+        Console.WriteLine("Введите сообщение: ");
+        string message = Console.ReadLine();
+
+        MyMessage myMessage = new MyMessage(type, message);
+        MessageManager messageManager = new MessageManager();
+
+        Phone phone = new Phone();
+        Mail mail = new Mail();
+
+        messageManager.NewMessage += phone.Message;
+        messageManager.NewMessage += mail.Message;
+        messageManager.TakeMessage(myMessage);
     }
 }
